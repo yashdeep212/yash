e123d172ee86cd0b5f4fb523d8f7b9ded4c98b63
amit  desktop <amit@gmail.com>
2019-04-09 18:35:31 +0530

    some chenges in addition file

--- addition.py
+++ addition.py
@@ -1,4 +1,15 @@
-a=input("enter the first no")
-b=input("enter the second no")
-c=a+b
-print("addition of a and b is=",c)
+def wordLength():
+    	pass    
+    wordcount={}
+    for word in st.split():        
+        key = len(word)
+
+        if key not in wordcount:
+            wordcount[key] = 1
+            
+        else:
+            wordcount[key] += 1
+    for k,v in wordcount.items():
+        print (k, v)
+    return None   
+wordLength("i my a yash n this is")
